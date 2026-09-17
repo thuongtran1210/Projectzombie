68f39c65198d74145d80997dad056e563d7d1a2b
Quang Thuong <thuongtran24092001@gmail.com>
2026-09-17 17:14:50 +0700

    fix(weapons): remove duplicate OnDisable and OnDestroy in Weapon_Pot

--- Assets/Features/Weapons/Weapon_Pot.cs
+++ Assets/Features/Weapons/Weapon_Pot.cs
@@ -120,16 +120,6 @@ namespace ProjectZombie.Features.Weapons
             UpdatePotFollowerMotion();
         }
 
-        private void OnDisable()
-        {
-            if (_potFollowerInstance != null) Destroy(_potFollowerInstance);
-        }
-
-        private void OnDestroy()
-        {
-            if (_potFollowerInstance != null) Destroy(_potFollowerInstance);
-        }
-
         #region POT FOLLOWER VISUAL (LƠ LỬNG SAU LƯNG HERO)
         private void SpawnPotFollower()
         {
